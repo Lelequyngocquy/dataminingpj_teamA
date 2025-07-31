0e809a19c4b11b2e5d73f557d0e9e17e1d0d6cc3
Lelequyngocquy <lengocquytdts@gmai.com>
2025-08-01 00:45:26 +0700

    box plot added

--- src/visualize.py
+++ src/visualize.py
@@ -13,12 +13,24 @@ def plot_target_distribution(df: pd.DataFrame, target_name: str):
         print(f"[WARN] Column '{target_name}' not found in DataFrame.")
         return
 
-    plt.figure(figsize=(8, 5))
-    sns.histplot(df[target_name].dropna(), kde=True, color="skyblue")
-    plt.title(f"Distribution of {target_name} - The last trained")
-    plt.xlabel(target_name)
+    data = df[target_name].dropna()
+
+    plt.figure(figsize=(10, 7))
+
+    # Subplot 1: Histogram + KDE
+    plt.subplot(2, 1, 1)
+    sns.histplot(data, kde=True, color="skyblue")
+    plt.title(f"Distribution of {target_name}")
+    plt.xlabel("")
     plt.ylabel("Frequency")
     plt.grid(True)
+    plt.xlabel(target_name)
+    # Subplot 2: Boxplot
+    plt.subplot(2, 1, 2)
+    sns.boxplot(x=data, color="lightcoral")
+
+    plt.grid(True)
+
     plt.tight_layout()
     plt.show()
 
